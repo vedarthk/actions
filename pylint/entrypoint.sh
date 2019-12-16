1947d1f5565241c9f389d07eb187e25343e026eb
vedarthk <vedarthk@vedarthz.in>
2019-12-16 13:32:48 +0530

    run pyllint on diff

--- pylint/entrypoint.sh
+++ pylint/entrypoint.sh
@@ -3,9 +3,13 @@
 
 lint() {
     pip install -r requirements.txt
-    pylint "${@}"
+    $diff=$(git diff ...recruiterbox_main --name-only | grep -E '\.py$')
+    if [ -n $diff ]
+      pylint $diff
+    fi
 }
 
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
 		lint "${@}"
 fi
+
